a4bd824ec7e4380686d79725009f759657aa7789
Cole Dahlen <coledahlen@gmail.com>
2023-02-07 11:46:46 -0600

    completed basemode

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -5,6 +5,7 @@ function ProfilePage() {
   const [uploadInput, setUploadInput] = useState('')
   const [nameInput, setNameInput] = useState('')
   const [bioInput, setBioInput] = useState('')
+  const [edit, setEdit] = useState(false)
   const user = useSelector(store => store.user)
   const dispatch = useDispatch();
   console.log(user.profile_url)
@@ -14,14 +15,14 @@ function ProfilePage() {
       profile_name: nameInput,
       profile_bio: bioInput
     }
-    if(user.profile_url !== null || user.profile_url !== ''){
+    if(user.profile_url){
       profileObject.profile_url = user.profile_url
     }
-    if(user.profile_name !== null || user.profile_name !== ''){
+    if(user.profile_name){
       profileObject.profile_name = user.profile_name
       
     }
-    if(user.profile_bio !== null || user.profile_bio !== ''){
+    if(user.profile_bio){
       profileObject.profile_bio = user.profile_bio
       
     }
@@ -30,40 +31,78 @@ function ProfilePage() {
       payload: profileObject
     })
   }
-  return (
-    <div className="container">
-      {
-        user.profile_url === null || user.profile_url === '' ? (
-        <div><input 
-        placeholder='Enter in a valid image link for your profile image'
-        value={uploadInput}
-        onChange={(event)=> setUploadInput(event.target.value)}
-        /></div>) : (<img className='profile_image' src={user.profile_url}/>)
-      }
-      {
-        user.profile_name === null || user.profile_name === '' ? (<div><input 
-        placeholder='Enter your name' 
-        value={nameInput}
-        onChange={(event)=> setNameInput(event.target.value)}
-        /></div>)
-        : (<div>{user.profile_name}</div>)
-      }
-      {
-        user.profile_bio === null || user.profile_bio === '' ? (<div><input
-        placeholder='Type a bio about yourself'
-        value={bioInput}
-        onChange={(event)=> setBioInput(event.target.value)}
-        /></div>) 
-        : (<div>{user.profile_bio}</div>)
-      }
-      { (user.profile_url === null || user.profile_name === null || user.profile_bio === null 
-      || user.profile_url === '' || user.profile_name === '' || user.profile_bio === '' )
-       ? <button onClick={handleSave}>Save</button> : <div></div> }
-      {
-        (user.)
-      }
-    </div>
-  );
+  const handleEdit = () =>{
+    setEdit(!edit)
+  }
+  const handleEditSave = () =>{
+    let profileObject = {
+      profile_url: uploadInput,
+      profile_name: nameInput,
+      profile_bio: bioInput
+    }
+    dispatch({
+      type: 'SAGA/UPDATE_USER',
+      payload: profileObject
+    })
+  }
+  if(edit === false){
+    return (
+      <div className="container">
+        {
+          !user.profile_url ? (
+          <div><input 
+          placeholder='Enter in a valid image link for your profile image'
+          value={uploadInput}
+          onChange={(event)=> setUploadInput(event.target.value)}
+          /></div>) : (<img className='profile_image' src={user.profile_url}/>)
+        }
+        {
+          !user.profile_name ? (<div><input 
+          placeholder='Enter your name' 
+          value={nameInput}
+          onChange={(event)=> setNameInput(event.target.value)}
+          /></div>)
+          : (<div>{user.profile_name}</div>)
+        }
+        {
+          !user.profile_bio ? (<div><input
+          placeholder='Type a bio about yourself'
+          value={bioInput}
+          onChange={(event)=> setBioInput(event.target.value)}
+          /></div>) 
+          : (<div>{user.profile_bio}</div>)
+        }
+        { (!user.profile_url || !user.profile_name || !user.profile_bio )
+         ? <button onClick={handleSave}>Save</button> : <div></div> }
+        {
+          (user.profile_url || user.profile_name || user.profile_bio)
+          ? <button onClick={handleEdit}>Edit</button> : <div></div>
+        }
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <input 
+          placeholder='Enter in a valid image link for your profile image'
+          value={uploadInput}
+          onChange={(event)=> setUploadInput(event.target.value)} 
+          />
+        <input 
+          placeholder='Enter your name' 
+          value={nameInput}
+          onChange={(event)=> setNameInput(event.target.value)}
+          />
+        <input
+          placeholder='Type a bio about yourself'
+          value={bioInput}
+          onChange={(event)=> setBioInput(event.target.value)}
+          />
+        <button onClick={handleEditSave}>Save</button>
+      </div>
+    )
+  }
+  
 }
 
 export default ProfilePage;
